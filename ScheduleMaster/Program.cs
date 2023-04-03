a4fee792d58a2f3725fd4a1d23641034c0e642bb
DefinitionP <121269108+DefinitionP@users.noreply.github.com>
2023-04-03 22:38:14 +0400

    Update Program.cs

--- ScheduleMaster/Program.cs
+++ ScheduleMaster/Program.cs
@@ -1,8 +1,8 @@
-﻿using ScheduleMaster;
-
-namespace ScheduleMaster
-{
-    class Program
+﻿using ScheduleMaster; // Техзадание дерьмо
+                        // Заказчики мудаки
+namespace ScheduleMaster// Разрабы нихуя не понимают
+{                       // Менеджеры хотят зарплату
+    class Program      // Скобелев хочет в Чикаго
     {
         static void Main(string[] args)
         {
@@ -13,14 +13,14 @@ namespace ScheduleMaster
                                 new Difficulty(6),
                                 false,
                                 false);
-            Employee loh1 = new Employee("Александр",
+            Employee huila = new Employee("Александр",
                                         "Александров",
                                         "Александрович",
                                         Gender.Male,
                                         Temperament.Sanguine,
                                         3,
                                         task1);
-            Console.WriteLine($"{loh1.Name} {loh1.Surname} {loh1.Patronymic}, назначена работа: {loh1.AssignedTask.Name}");
+            Console.WriteLine($"{huila.Name} {loh1.Surname} {huila.Patronymic}, назначена работа: {huila.AssignedTask.Name}");
             Console.ReadLine();
         }
     }
